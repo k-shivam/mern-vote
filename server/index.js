8dcb6fa0ab7873d48199f22e5e2c40bdc156fee2
shivam <shivam@gramfactory.com>
2020-07-17 00:11:57 +0530

    added logic for login and register

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
+require('dotenv').config();
+
 var express = require('express');
 
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const handle = require('./handlers');
+const routes = require('./routes');
+const db = require('./models');
 
 const app = express();
-const port =4000;
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => res.json({hello: 'world'}));
+app.use('/api/auth', routes.auth);
 
 app.use(handle.notFound);
 
